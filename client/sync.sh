b7ca0943a6b3b51ba9ce22802c5484ad9723c563
Michael Gibney <michael@michaelgibney.net>
2015-05-21 09:54:48 -0400

    added logging and bailout for failed commands

--- client/sync.sh
+++ client/sync.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
+die() {
+  echo "$1" >&2
+  exit 1
+}
+
+case "$1" in
+   update)
+     # pass through to server update script
+     shift
+   ;;
+   '')
+     git commit -am 'sync auto-message' || die "sync auto-commit failed"
+     git push || die "sync auto-push failed"
+     exit
+   ;;
+   *)
+     echo "unrecognized options: $1" >&2
+     exit 1
+   ;;
+esac
+
 REMOTE_URL=$(git config --get remote.origin.url)
 SSH_CONNECT="${REMOTE_URL%:*}"
 REMOTE_PATH="${REMOTE_URL#*:}"
-REMOTE_UPDATE_SCRIPT="./.git/server-sync/server/update.sh"
+REMOTE_UPDATE_SCRIPT="sync/server/update.sh"
 
 if [ -z "$SSH_CONNECT" ]; then
-  ( cd "$REMOTE_PATH" && "$REMOTE_UPDATE_SCRIPT" "${@}" )
+  ( cd "$REMOTE_PATH" && "${GIT_DIR:-./.git}/$REMOTE_UPDATE_SCRIPT" "${@}" ) || die "local remote command failed"
 else
-  ssh "$SSH_CONNECT" "cd "$REMOTE_PATH" && "$REMOTE_UPDATE_SCRIPT" "${@}"" 
+  ssh "$SSH_CONNECT" "cd "$REMOTE_PATH" && "\${GIT_DIR:-./.git}/$REMOTE_UPDATE_SCRIPT" "${@}"" || die "remote command failed"
 fi
 
 REMOTE_WORK_BRANCH="origin/work"
 BACKUP_TAG="work_backup"
-git fetch --all
+git fetch --all || die "client fetch command failed"
 git tag -d "$BACKUP_TAG"
 git tag "$BACKUP_TAG"
-git reset --keep "$REMOTE_WORK_BRANCH"
+git reset --keep "$REMOTE_WORK_BRANCH" || die "client \"reset --keep\" command failed"
--- server/update.sh
+++ server/update.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $# -gt 1 ]; then
-  echo "too many arguments: \"${*}\"" >&2
+die() {
+  echo "$1" >&2
   exit 1
+}
+
+if [ $# -gt 1 ]; then
+  die "too many arguments: \"${*}\""
 fi
 
 DEV_BRANCH="dev"
@@ -13,20 +17,21 @@ COMMIT_MESSAGE="$1"
 if [ -z "$COMMIT_MESSAGE" ]; then
   COMMIT_MESSAGE="update auto-message"
 fi
-git fetch --all
+git fetch --all || die "remote fetch failed"
 git tag -d "$BACKUP_TAG"
 git tag "$BACKUP_TAG"
 git branch "$TMP_BRANCH" "$DEV_BRANCH"
-git rebase "$TMP_BRANCH"
+git rebase "$TMP_BRANCH" || die "remote rebase failed"
 git checkout "$TMP_BRANCH"
-git merge --squash "$WORK_BRANCH"
+git merge --squash "$WORK_BRANCH" || die "remote squash merge failed"
 git commit -m "$COMMIT_MESSAGE"
-git checkout "$WORK_BRANCH"
-git reset --hard $(git merge-base "$WORK_BRANCH" "$TMP_BRANCH")
-git merge --ff-only "$TMP_BRANCH"
+git checkout "$WORK_BRANCH" || die "failed to checkout $WORK_BRANCH on remote"
+MERGE_BASE=$(git merge-base "$WORK_BRANCH" "$TMP_BRANCH") || die "failed to get merge-base on remote"
+git reset --hard "$MERGE_BASE" || die "remote hard reset failed (merge-base: $MERGE_BASE)"
+git merge --ff-only "$TMP_BRANCH" || die "failed to merge $TMP_BRANCH into $WORK_BRANCH on remote"
 if [ -n "$1" ]; then
-  git checkout "$DEV_BRANCH"
-  git merge --ff-only "$TMP_BRANCH"
-  git checkout "$WORK_BRANCH"
+  git checkout "$DEV_BRANCH" || die "remote failed to checkout $DEV_BRANCH"
+  git merge --ff-only "$TMP_BRANCH" || die "failed to merge $TMP_BRANCH into $DEV_BRANCH on remote"
+  git checkout "$WORK_BRANCH" || die "failed to checkout $WORK_BRANCH on remote"
 fi
-git branch -d "$TMP_BRANCH"
+git branch -d "$TMP_BRANCH" || die "failed to delete $TMP_BRANCH"
