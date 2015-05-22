df3b17f5fa75f42b5468b99db2dc29ffa8b60164
Michael Gibney <michael@michaelgibney.net>
2015-05-22 16:05:21 -0400

    fixed some init issues for starting from scratch

--- init.sh
+++ init.sh
@@ -12,13 +12,11 @@ die() {
 }
 
 create-work-branch() {
-  branch_name="$(git symbolic-ref HEAD 2>/dev/null)" || die "no branch found; checkout a branch and re-init"
-
-  if ! git rev-parse "refs/heads/$WORK_BRANCH" >/dev/null 2>&1; then
-    git --work-tree="../" branch "$WORK_BRANCH" "$branch_name" || die "failed checkout -b ($PWD)"
+  if git rev-parse "refs/heads/$WORK_BRANCH" >/dev/null 2>&1 || [ "$1" != "server" ]; then
+    git --work-tree="../" checkout "$WORK_BRANCH" || die "failed checkout ($PWD)"
+  else
+    git --work-tree="../" checkout -b "$WORK_BRANCH" || die "failed checkout -b ($PWD)"
   fi
-  git --work-tree="../" checkout "$WORK_BRANCH" || die "failed checkout ($PWD)"
-  echo "$branch_name" > "$WORK_HEAD_FILE"
 }
 
 (
@@ -31,12 +29,17 @@ case "$1" in
     git config receive.denyCurrentBranch ignore
     git config alias.sync "!${GIT_DIR:-./.git}/sync/server/sync.sh"
     ln -s ../sync/server/hooks/post-update hooks/post-update
-    create-work-branch
+    branch_name="$(git symbolic-ref HEAD 2>/dev/null)" || die "no branch found; checkout a branch and re-init"
+    create-work-branch server
+    echo "$branch_name" > "$WORK_HEAD_FILE"
   ;;
   client)
     echo "configure for client"
     git config alias.sync "!${GIT_DIR:-./.git}/sync/client/sync.sh"
     create-work-branch
+    git --work-tree="../" sync update
+    export GIT_WORK_TREE="$PWD/../"
+    git sync force-local-update
   ;;
   *)
     echo "unrecognized option: $1" >&2
